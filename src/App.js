f24963fbcbc1153d3caad57930ee11f2ae77b23a
jkar <gianniskaramitsos3@gmail.com>
2020-10-01 23:24:10 +0300

    getting params

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Products from './components/products';
+import axios from 'axios';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const [totalPages, setTotalPages] = useState();
+  const [limit, setLimit] = useState(4);
+
+  console.log('totalPages', totalPages);
+  console.log('products', products);
+
+  const requestPageProduct = (lmt, ofst) => {
+    console.log('ofs', ofst)
+    const params = {
+      limit : parseInt(lmt),
+      offset : parseInt(ofst)
+    }
+    const request = axios.get('http://localhost:3001/api/products', { params : params } );
+    request.then( res => {
+      setProducts(res.data);
+    });
+  };
+
+  const requestTotalPages = (l) => {
+    const params = {
+      limit : limit
+    }
+    const request = axios.get('http://localhost:3001/api/products/count', { params: params });
+    request.then( res => {
+      requestPageProduct(limit, 0);
+      setTotalPages(res.data.numberOfPages);
+    })
+  };
+
+  useEffect(() => {
+    requestTotalPages(limit);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Products />
     </div>
   );
 }
